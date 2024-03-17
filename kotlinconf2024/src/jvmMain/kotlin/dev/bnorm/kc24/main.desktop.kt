fdc756e82aecf3464e5fbb379ba8dfca68135d5d
Brian Norman <brian.norman@jetbrains.com>
2024-03-17 12:32:01 -0500

    Updates for desktop and web slide shows

--- kotlinconf2024/src/jvmMain/kotlin/dev/bnorm/kc24/main.desktop.kt
+++ kotlinconf2024/src/jvmMain/kotlin/dev/bnorm/kc24/main.desktop.kt
@@ -1,12 +1,15 @@
 package dev.bnorm.kc24
 
+import androidx.compose.ui.window.application
 import dev.bnorm.librettist.DesktopSlideShow
 import dev.bnorm.librettist.show.ShowBuilder
 
 fun main() {
-    DesktopSlideShow(
-        title = "Kotlin + Power-Assert = Love",
-        theme = { Theme.dark },
-        builder = ShowBuilder::KotlinPlusPowerAssertEqualsLove
-    )
+    application {
+        DesktopSlideShow(
+            title = "Kotlin + Power-Assert = Love",
+            theme = Theme.dark,
+            builder = ShowBuilder::KotlinPlusPowerAssertEqualsLove
+        )
+    }
 }
--- kotlinconf2024/src/wasmJsMain/kotlin/main.web.kt
+++ kotlinconf2024/src/wasmJsMain/kotlin/main.web.kt
@@ -1,12 +1,16 @@
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
 import dev.bnorm.kc24.KotlinPlusPowerAssertEqualsLove
 import dev.bnorm.kc24.Theme
-import dev.bnorm.librettist.WebSlideShow
+import dev.bnorm.librettist.EmbeddedSlideShow
 import dev.bnorm.librettist.show.ShowBuilder
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    WebSlideShow(
-        canvasElementId = "ComposeTarget",
-        theme = { Theme.dark },
-        builder = ShowBuilder::KotlinPlusPowerAssertEqualsLove
-    )
+    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+        EmbeddedSlideShow(
+            theme = Theme.dark,
+            builder = ShowBuilder::KotlinPlusPowerAssertEqualsLove
+        )
+    }
 }
