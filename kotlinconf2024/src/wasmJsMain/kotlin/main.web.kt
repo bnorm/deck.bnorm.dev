175cce387d203b9981a295b04e4e4889ac778e79
Brian Norman <brian.norman@jetbrains.com>
2024-04-19 07:41:57 -0500

    Use a query param to link to a specific slide

--- kotlinconf2024/src/wasmJsMain/kotlin/main.web.kt
+++ kotlinconf2024/src/wasmJsMain/kotlin/main.web.kt
@@ -4,12 +4,16 @@ import dev.bnorm.kc24.KotlinPlusPowerAssertEqualsLove
 import dev.bnorm.kc24.Theme
 import dev.bnorm.librettist.EmbeddedSlideShow
 import dev.bnorm.librettist.show.ShowBuilder
+import kotlinx.browser.window
+import org.w3c.dom.url.URLSearchParams
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    val params = URLSearchParams(window.location.search.toJsString())
     CanvasBasedWindow(canvasElementId = "ComposeTarget") {
         EmbeddedSlideShow(
             theme = Theme.dark,
+            startSlide = params.get("slide")?.toIntOrNull() ?: 0,
             builder = ShowBuilder::KotlinPlusPowerAssertEqualsLove
         )
     }
