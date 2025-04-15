3f63e14d6cc03533510799f76875690bfea94cc1
Brian Norman <brian.norman@jetbrains.com>
2025-04-15 09:20:46 -0500

    Upgrade to Kotlin 2.2.0-Beta1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,20 +5,17 @@ pluginManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
-        maven("https://redirector.kotlinlang.org/maven/bootstrap")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://packages.jetbrains.team/maven/p/firework/dev")
     }
 
     plugins {
-        val kotlinVersion = "2.2.0-dev-12941"
+        val kotlinVersion = "2.2.0-Beta1"
 
         kotlin("multiplatform") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         kotlin("plugin.compose") version kotlinVersion
         kotlin("plugin.power-assert") version kotlinVersion
         id("org.jetbrains.compose") version "1.8.0-beta02"
-        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha05-91"
+        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha05"
     }
 }
 
@@ -26,9 +23,6 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         google()
-        maven("https://redirector.kotlinlang.org/maven/bootstrap")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://packages.jetbrains.team/maven/p/firework/dev")
     }
 }
 
