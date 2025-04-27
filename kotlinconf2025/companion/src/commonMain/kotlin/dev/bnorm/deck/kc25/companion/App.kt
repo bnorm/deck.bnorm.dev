f6547d51e5c68412f66f179a1e0894de28196641
Brian Norman <brian.norman@jetbrains.com>
2025-04-27 08:59:39 -0500

    A lazy column isn't needed in the companion

--- kotlinconf2025/companion/src/commonMain/kotlin/dev/bnorm/deck/kc25/companion/App.kt
+++ kotlinconf2025/companion/src/commonMain/kotlin/dev/bnorm/deck/kc25/companion/App.kt
@@ -1,18 +1,19 @@
 package dev.bnorm.deck.kc25.companion
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.bnorm.deck.kc25.companion.cards.EmbeddedStory
 import dev.bnorm.deck.kc25.companion.cards.SendAReaction
@@ -29,52 +30,44 @@ fun BroadcastMessage.toStoryboard(): Storyboard.Index {
 // TODO the app seems to become non-responsive on Android Chrome after the phone goes to sleep
 @Composable
 fun App(storyState: StoryState) {
-    MaterialTheme(colors = lightColors()) {
-        val state = rememberLazyListState()
-        val maxIndex = rememberMaxIndex(storyState)
+    // TODO switch between light and dark themes?
+    MaterialTheme(colors = lightColors(background = Color.LightGray)) {
+        val state = rememberScrollState()
 
         Box(
             contentAlignment = Alignment.TopCenter,
             modifier = Modifier.fillMaxSize()
+                .background(MaterialTheme.colors.background)
                 .scrollable(state, orientation = Orientation.Vertical, reverseDirection = true)
         ) {
-            LazyColumn(
-                state = state,
+            Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                contentPadding = PaddingValues(16.dp),
                 modifier = Modifier
+                    .verticalScroll(state)
+                    .padding(16.dp)
                     .fillMaxHeight()
                     // TODO can i make the content scrollable once smaller than min width?
                     .widthIn(max = 960.dp)
                     .requiredWidthIn(min = 720.dp)
             ) {
-                Content(maxIndex, storyState)
+                Content(storyState)
             }
         }
     }
 }
 
-private fun LazyListScope.Content(
-    latest: State<Storyboard.Index>,
-    storyState: StoryState,
-) {
-    // TODO make some of these sticky?
-    item("title") {
-        ContentCard(Modifier.animateItem()) {
-            Title()
-        }
+@Composable
+private fun ColumnScope.Content(storyState: StoryState) {
+    ContentCard {
+        Title()
     }
 
-    item("story") {
-        ContentCard(Modifier.animateItem()) {
-            EmbeddedStory(storyState)
-        }
+    ContentCard(Modifier.padding(top = 16.dp)) {
+        EmbeddedStory(storyState)
     }
 
-    item("Reactions") {
-        ContentCard(Modifier.animateItem()) {
-            SendAReaction()
-        }
+    ContentCard(Modifier.padding(top = 16.dp)) {
+        SendAReaction()
     }
 
     // TODO ideas
@@ -91,9 +84,10 @@ private fun LazyListScope.Content(
     //  * [ ] card with link to test documentation:
     //    - https://github.com/JetBrains/kotlin/blob/v2.2.0-Beta1/compiler/test-infrastructure/ReadMe.md
 
-    repeat(latest.value.sceneIndex) {
-        item(it) {
-            ContentCard(Modifier.animateItem()) {
+    val maxIndex = rememberMaxIndex(storyState)
+    repeat(50) {
+        AnimatedVisibility(maxIndex.value.sceneIndex >= it) {
+            ContentCard(Modifier.padding(top = 16.dp)) {
                 Text("Information ${it + 1}", style = MaterialTheme.typography.h2)
             }
         }
