4fe7f15df6e7c6f5e48bf8901c6eaf514069a352
Brian Norman <brian.norman@jetbrains.com>
2025-06-09 19:45:49 -0500

    Add all the KotlinConf 2024 old animation code

---
+++ dcnyc25/story/src/commonMain/kotlin/dev/bnorm/dcnyc25/old/kc24/AnimationSequence.kt
@@ -0,0 +1,42 @@
+package dev.bnorm.dcnyc25.old.kc24
+
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
+data class AnimationSequence<T>(
+    val start: T,
+    val end: T,
+    val sequence: Sequence<T>,
+) {
+    fun toList(): ImmutableList<T> {
+        return sequence.deduplicate().toImmutableList()
+    }
+}
+
+fun <T> startAnimation(start: T): AnimationSequence<T> {
+    val sequence = sequence { yield(start) }
+    return AnimationSequence(start, start, sequence)
+}
+
+fun <T> AnimationSequence<T>.then(next: T): AnimationSequence<T> {
+    val sequence = sequence { yieldAll(sequence); yield(next) }
+    return copy(end = next, sequence = sequence)
+}
+
+fun <T> AnimationSequence<T>.then(next: AnimationSequence<T>): AnimationSequence<T> {
+    val sequence = sequence { yieldAll(sequence); yieldAll(next.sequence) }
+    return copy(end = next.end, sequence = sequence)
+}
+
+fun <T> Sequence<T>.deduplicate(): Sequence<T> {
+    val upstream = this
+    return sequence {
+        var prev: Any? = Any()
+        for (it in upstream) {
+            if (it != prev) {
+                yield(it)
+            }
+            prev = it
+        }
+    }
+}
