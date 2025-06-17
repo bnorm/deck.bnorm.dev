4b2d66788da61f41484d0fe6a9e075596f4202f7
Brian Norman <brian.norman@jetbrains.com>
2025-06-17 16:51:58 -0500

    Delete unused animation code

--- dcnyc25/story/src/commonMain/kotlin/dev/bnorm/dcnyc25/old/kc24/AnimateListAsState.kt
+++ dcnyc25/story/src/commonMain/kotlin/dev/bnorm/dcnyc25/old/kc24/AnimateListAsState.kt
@@ -7,27 +7,6 @@ import androidx.compose.runtime.derivedStateOf
 import kotlinx.collections.immutable.ImmutableList
 
 @Composable
-fun <T> animateListAsState(
-    targetIndex: Int,
-    values: ImmutableList<T>,
-    animationSpec: AnimationSpec<Int> = tween(
-        durationMillis = 1_000,
-        easing = LinearEasing
-    ),
-    label: String = "ListAnimation",
-    finishedListener: ((T) -> Unit)? = null,
-): State<T> {
-    val index = animateIntAsState(
-        targetValue = targetIndex,
-        animationSpec = animationSpec,
-        label = label,
-        finishedListener = finishedListener?.let { listener -> { listener(values[it]) } }
-    )
-    // TODO is this the best way to map a state object?
-    return derivedStateOf { values[index.value] }
-}
-
-@Composable
 inline fun <S, T> Transition<S>.animateList(
     values: ImmutableList<T>,
     noinline transitionSpec: @Composable Transition.Segment<S>.() -> FiniteAnimationSpec<Int> = {
