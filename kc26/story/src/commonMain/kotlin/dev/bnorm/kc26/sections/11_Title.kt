fa41cbd86d1adc77410bb8f6169975898eb0260f
Brian Norman <brian@bnorm.com>
2026-03-23 11:10:37 -0500

    Align with KotlinConf template as best as possible
    May adjust later to match personal style and template. But for now, try
    and align with the KotlinConf template as much as possible so we have it
    as a starting point.

--- kc26/story/src/commonMain/kotlin/dev/bnorm/kc26/sections/11_Title.kt
+++ kc26/story/src/commonMain/kotlin/dev/bnorm/kc26/sections/11_Title.kt
@@ -8,51 +8,49 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import dev.bnorm.deck.shared.socials.Bluesky
+import androidx.compose.ui.unit.sp
+import dev.bnorm.deck.shared.ResourceImage
 import dev.bnorm.deck.shared.socials.JetBrainsEmployee
-import dev.bnorm.deck.shared.socials.Mastodon
+import dev.bnorm.deck.story.generated.resources.Res
+import dev.bnorm.deck.story.generated.resources.conf_logo
+import dev.bnorm.deck.story.generated.resources.conf_title
 import dev.bnorm.kc26.template.carouselScene
 import dev.bnorm.storyboard.StoryboardBuilder
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.job
 
 fun StoryboardBuilder.Title() {
     carouselScene {
         Box(Modifier.fillMaxSize()) {
+            ResourceImage(
+                Res.drawable.conf_title,
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(vertical = 42.dp, horizontal = 39.dp)
+                    .height(40.dp)
+            )
+            ResourceImage(
+                Res.drawable.conf_logo,
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .padding(vertical = 40.dp, horizontal = 32.dp)
+            )
             Column(
                 Modifier.fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(32.dp)
                     .align(Alignment.BottomStart)
             ) {
-                Column(Modifier.padding(16.dp)) {
-                    ProvideTextStyle(MaterialTheme.typography.h2.copy(fontWeight = FontWeight.SemiBold)) {
-                        Text("Powering Up") // TODO lightning emoji?
-                        Text("Your Assertions") // TODO magnifying glass emoji?
-                    }
-                }
-                Row(
-                    verticalAlignment = Alignment.Bottom,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.Start,
-                        modifier = Modifier.padding(16.dp),
-                    ) {
-                        JetBrainsEmployee(
-                            name = "Brian Norman",
-                            title = "Kotlin Compiler Developer",
+                Column(Modifier.padding(0.dp)) {
+                    ProvideTextStyle(
+                        MaterialTheme.typography.h2.copy(
+                            fontWeight = FontWeight.SemiBold,
+                            lineHeight = 60.sp,
                         )
-                    }
-                    Spacer(Modifier.weight(1f))
-                    Column(
-                        horizontalAlignment = Alignment.Start,
-                        modifier = Modifier.padding(16.dp),
                     ) {
-                        Mastodon(username = "bnorm@kotlin.social")
-                        Spacer(Modifier.size(4.dp))
-                        Bluesky(username = "@bnorm.dev")
+                        Text("Powering\nUp Your\nAssertions")
                     }
+                    JetBrainsEmployee(
+                        name = "Brian Norman",
+                        title = "Kotlin Compiler Developer",
+                    )
                 }
             }
         }
--- kc26/story/src/commonMain/kotlin/dev/bnorm/kc26/sections/91_Closing.kt
+++ kc26/story/src/commonMain/kotlin/dev/bnorm/kc26/sections/91_Closing.kt
@@ -1,32 +1,104 @@
 package dev.bnorm.kc26.sections
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.lerp
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
+import dev.bnorm.deck.shared.ResourceImage
 import dev.bnorm.deck.shared.socials.Bluesky
 import dev.bnorm.deck.shared.socials.Mastodon
+import dev.bnorm.deck.story.generated.resources.Res
+import dev.bnorm.deck.story.generated.resources.badge
+import dev.bnorm.deck.story.generated.resources.closing
+import dev.bnorm.deck.story.generated.resources.phone
 import dev.bnorm.kc26.template.carouselScene
 import dev.bnorm.storyboard.StoryboardBuilder
 
 fun StoryboardBuilder.Closing() {
     carouselScene {
         Box(modifier = Modifier.fillMaxSize()) {
-            Column(Modifier.align(Alignment.TopStart).padding(32.dp)) {
-                Mastodon(username = "bnorm@kotlin.social")
-                Spacer(Modifier.size(4.dp))
+            ResourceImage(
+                Res.drawable.badge,
+                modifier = Modifier
+                    .size(218.7f.dp, 401.4f.dp)
+                    .offset(472.dp, 0.dp)
+            )
+
+            // TODO there's a subtle border around the phone in the template
+            //  due to another background image behind the phone from 2025
+            ResourceImage(
+                Res.drawable.phone,
+                modifier = Modifier
+                    .size(217.4f.dp, 401.4f.dp)
+                    .offset(716f.dp, 62.7f.dp)
+            )
+
+            DrawArc(1.2f)
+
+            Column(Modifier.align(Alignment.TopStart).padding(start = 40.dp, top = 40.dp)) {
                 Bluesky(username = "@bnorm.dev")
+                Spacer(Modifier.size(4.dp))
+                Mastodon(username = "bnorm@kotlin.social")
             }
-            Column(Modifier.align(Alignment.BottomStart).padding(32.dp)) {
-                ProvideTextStyle(MaterialTheme.typography.h2.copy(fontWeight = FontWeight.SemiBold)) {
-                    Text("Thank You,")
-                    Text("and Don't")
-                    Text("Forget to Vote")
+            Column(Modifier.align(Alignment.BottomStart).padding(start = 40.dp, bottom = 28.dp)) {
+                ProvideTextStyle(
+                    MaterialTheme.typography.h2.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 54.sp,
+                        lineHeight = 54.sp,
+                        color = Color.Blue,
+                    )
+                ) {
+                    Text("Thank You,\nand Don't\nForget to Vote")
                 }
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun DrawArc(fraction: Float) {
+    Canvas(Modifier.fillMaxSize()) {
+        val xEnd = 1488.0f
+        val yEnd = 962.0f
+        val topLeft = Offset(1168.0f, 620.0f)
+        val size = Size(388.0f, 388.0f)
+
+        val brush = Brush.horizontalGradient(
+            startX = topLeft.x,
+            endX = xEnd,
+            colors = listOf(
+                Color(0xFFE901FF),
+                Color(0xFFFD061C),
+            )
+        )
+
+        if (fraction > 0f) {
+            val angleFraction = fraction.coerceAtMost(1f)
+            val angle = lerp(0f, -130f, angleFraction)
+            drawArc(brush, 180f, angle, false, topLeft, size, style = Stroke(5.0f))
+
+            if (fraction > 1f) {
+                val arrowFraction = (fraction - 1f).coerceAtLeast(0f) * 5f
+                val left = lerp(Offset(xEnd, yEnd), Offset(xEnd - 27, yEnd - 3), arrowFraction)
+                val right = lerp(Offset(xEnd, yEnd), Offset(xEnd - 3, yEnd + 27), arrowFraction)
+                drawLine(brush, Offset(xEnd + 1, yEnd), left, strokeWidth = 5.0f)
+                drawLine(brush, Offset(xEnd, yEnd - 1), right, strokeWidth = 5.0f)
             }
         }
     }
