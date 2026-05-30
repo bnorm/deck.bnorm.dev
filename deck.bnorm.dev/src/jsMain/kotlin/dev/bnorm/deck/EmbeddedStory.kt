ab759ad0e9299a290e4b5301b925acb2c363e376
Brian Norman <brian@bnorm.com>
2026-05-30 15:24:47 -0500

    Only load talk slides when requested

--- deck.bnorm.dev/src/jsMain/kotlin/dev/bnorm/deck/EmbeddedStory.kt
+++ deck.bnorm.dev/src/jsMain/kotlin/dev/bnorm/deck/EmbeddedStory.kt
@@ -1,16 +1,34 @@
 package dev.bnorm.deck
 
-import androidx.compose.runtime.Composable
-import org.jetbrains.compose.web.css.height
-import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.css.width
+import androidx.compose.runtime.*
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Iframe
+import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun EmbeddedStory(storyId: String) {
-    Iframe({
-        classes("talk-deck")
-        attr("src", "https://deck.bnorm.dev/$storyId")
-        attr("frameborder", "0")
-    })
+    var loaded by remember { mutableStateOf(false) }
+    if (loaded) {
+        Iframe({
+            classes("talk-deck")
+            attr("src", "https://deck.bnorm.dev/$storyId")
+            attr("frameborder", "0")
+        })
+    } else {
+        Div({
+            classes("talk-deck")
+            onClick {
+                loaded = true
+            }
+            style {
+                display(DisplayStyle.Grid)
+                property("place-items", "center")
+                cursor("pointer")
+                border(width = 2.px, style = LineStyle.Solid, color = Color.white)
+            }
+        }) {
+            Text("Click to load slides")
+        }
+    }
 }
